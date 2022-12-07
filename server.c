699c63790fb99471420168cdb4e3bcd03afe8f9b
kevinPJdev <kevjacobdev#gmail.com>
2022-12-06 19:28:31 -0500

    update client.c

--- server.c
+++ server.c
@@ -12,10 +12,11 @@
 int main(int argc, char *argv[]) {
 
     struct sockaddr_in server_addr, client; //declare socket address structure
-    int sock1, ret, l; //To handle sock commands
+    int sockRet, bindRet, listenRet, acceptRet; //To handle sock commands
+    socklen_t clientLen;
 
-    if((sock1 = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        printf("------Socket Creation Failed------");
+    if((sockRet = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("\n------Socket Creation Failed------\n");
         exit(1);
     }
 
@@ -26,17 +27,33 @@ int main(int argc, char *argv[]) {
     //setting internet address
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if((ret = bind(sock1, (struct sockaddr*)&server_addr, sizeof(server_addr))) < 0) {
-         printf("------Socket Binding Failed------");
+    //bind() is usually called by a server to bind their local  IP and a well-known port number to a socket
+    if((bindRet = bind(sockRet, (struct sockaddr*)&server_addr, sizeof(server_addr))) < 0) {
+         printf("\n------Socket Binding Failed------\n");
          exit(1);
     }
 
-    if((l = listen(sock1, 10)) < 0) {
-        printf("------Listener Failed------");
+    //listen() is called only by a TCP server to accept  connections from client sockets that will issue a  connect().
+    if((listenRet = listen(sockRet, 10)) < 0) {
+        printf("\n------Listener Failed------\n");
         exit(1);        
     }       
 
-    while(1) {    
+    while(1) {
+        printf("\n------Waiting for pending connections------\n");
+        clientLen = sizeof(client);
+
+        /* 
+        *  accept() is called by a TCP server to extract the first  connection in the queue of pending connections,  
+        *  creates a new socket with the properties of s, and  allocates a new file descriptor for the newly created socket.
+        */
+        if((acceptRet = accept(sockRet, (struct sockaddr*)&client, &clientLen)) < 0) {
+            printf("\n-------Socket Connection Failed--------\n");
+            exit(1);
+        }
+
+
+
     }
 
 
