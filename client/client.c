8168fba2f660a06fca186594151e5ae337639c6d
kevinPJdev <kevjacobdev#gmail.com>
2022-12-07 20:56:03 -0500

    multi server conns

---
+++ client/client.c
@@ -0,0 +1,210 @@
+/*
+ * Authors
+ * Kevin Patrick Jacob
+ * Harmanjot Singh Suri
+ */
+
+#include <strings.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/*for getting file size using stat()*/
+#include<sys/stat.h>
+
+/*for O_RDONLY*/
+#include<fcntl.h>
+
+#define COMMAND(cmd) strcmp(command, cmd)==0
+
+#define PORT1 5090
+#define PORT2 5091
+
+int main(int argc,char *argv[])
+{
+	char choice[256], arg[256]; //Variable choice for handling user choice
+	FILE *fd, *fptr1, *fptr2; //File Descriptor
+	struct stat obj;  // Obj variable to keep the count 
+	char buf[100], command[5], filename[20], *f; //Character Arrays
+	int k, size, status;
+	int filehandle;
+	int count_serverA, count_serverB;
+	int portCondA = 0; //defines whether client connects to Server A or Server B
+
+	//Read the number of connections from server A
+	fptr1 = fopen("serverAComm.txt", "r");
+	fscanf(fptr1, "%d", &count_serverA);
+	count_serverA++;
+	printf("There are currently %d connections to server A\n", count_serverA);
+
+	//Read the number of connections from server B
+	fptr2 = fopen("serverBComm.txt", "r");
+	fscanf(fptr2, "%d", &count_serverB);
+	count_serverB++;
+	printf("There are currently %d connections to server B\n", count_serverB);
+
+	if(count_serverA <= 5) {
+		portCondA = 1;
+	} else if(count_serverA > 5 && count_serverB <= 5) {
+		portCondA = 0;
+	} 
+
+
+	// TCP Protocol
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock == -1){
+		printf("socket creation failed\n");
+		exit(1);
+	}
+	printf("socket creation successfully\n");
+
+	struct sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+
+	// set server IP address
+	server_addr.sin_addr.s_addr = INADDR_ANY; // Server IP Address
+
+	// set server port number
+	server_addr.sin_port = htons(portCondA == 1? PORT1 : PORT2); //Server Port
+
+	int e = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)); //Create Connection
+	if(e == -1){
+        perror("Error in Connecting");
+        exit(1);
+    }
+    printf("Connected to server.\n");
+	int i = 1;
+
+	while(1)
+	{
+		/*==============================================DISPLAY-MENU===============================================*/
+		printf("\nEnter a choice :\n1- RETR\n2- STOR\n7- PWD\n8- MKD\n9- RMD\n10- LIST\n11- CWD\n12- USER\n15- DELE\n16- QUIT\n17- CDUP\n18- REIN\n19- REST\n20- PORT\n21- STAT\n");
+		scanf("%s", choice);
+
+		if(strcmp(choice, "USER") == 0){
+
+			// get from system
+			int name = system("whoami");
+			printf("User name ok.\n");
+		}
+
+		else if(strcmp(choice, "STOR") == 0){
+			printf("Enter filename to STOR to server: ");
+
+			// Get the input from user
+			scanf("%s", filename);
+
+			// Open file in read only mode
+			filehandle = open(filename, O_RDONLY);
+
+			// check file is exist or not
+			if(filehandle == -1)
+			{
+				printf("No such file on the local directory\n\n");
+				//break;
+			}
+		}
+		/* Get the present working directory */
+		else if(strcmp(choice, "PWD") == 0){
+
+			// Copy the command
+			strcpy(buf, "PWD");
+
+			// send command to server
+			send(sock, buf, 100, 0);
+
+			// get the working directory
+			recv(sock, buf, 100, 0);
+			printf("The path of the remote directory is: %s\n", buf);
+		}
+		/* Make a new directory */
+		else if(strcmp(choice, "MKD") == 0){
+
+			// Copy the command
+			strcpy(buf, "MKD");
+
+			// send command to server
+			send(sock, buf, 100, 0);
+
+			printf("Enter the directory name to create SERVER side: ");
+
+			// get input from user
+			scanf("%s", filename);
+			send(sock, filename, 100, 0);
+
+			bzero(buf, sizeof(buf));
+
+			// Receive what created
+			recv(sock, buf, 100, 0);
+			printf("The path of the remote directory is: %s\n", buf);
+		}
+		/* Remove a directory */
+		else if(strcmp(choice, "RMD") == 0){
+			strcpy(buf, "RMD");
+			send(sock, buf, 100, 0);
+
+			printf("Enter the directory name to delete on SERVER side: ");
+			scanf("%s", filename);
+			send(sock, filename, 100, 0);
+
+			bzero(buf, sizeof(buf));
+			recv(sock, buf, 100, 0);
+			printf("Directory Removed Successfully\n");
+		}
+		/* Delete a file from server */
+		else if(strcmp(choice, "DELE") == 0){
+			strcpy(buf, "DELE");
+			send(sock, buf, 100, 0);
+
+			printf("Enter the file name to delete on SERVER side: ");
+			scanf("%s", filename);
+			send(sock, filename, 100, 0);
+
+			bzero(buf, sizeof(buf));
+			recv(sock, buf, 100, 0);
+			printf("[+]File Removed Successfully\n");
+		}
+		/* List all files in the PWD*/
+		else if(strcmp(choice, "LIST") == 0){
+			strcpy(buf, "LIST");
+			send(sock, buf, 100, 0);
+			recv(sock, buf, 1024, 0);
+			printf("The remote directory listing is as follows:\n %s\n\n", buf);
+			//system();
+		}
+		/* Change the current working directory */
+		else if(strcmp(choice, "CWD") == 0){
+			strcpy(buf, "CD ");
+			printf("Enter the path to change the remote directory: ");
+			scanf("%s", buf + 3);
+			send(sock, buf, 100, 0);
+			recv(sock, &status, sizeof(int), 0);
+			if(status)
+				printf("Remote directory successfully changed\n");
+			else
+				printf("Remote directory failed to change\n");
+		}
+		/* Quit and close the connection */
+		else if(strcmp(choice, "QUIT") == 0){
+			strcpy(buf, "QUIT");
+			send(sock, buf, 100, 0);
+			recv(sock, &status, 100, 0);
+			if(status){
+				printf("Connection closed\nQuitting..\n");
+				printf("Terminated Successfully...\nGood Bye...\n");
+				close(sock);
+				exit(0);
+			}
+			printf("[-]Server failed to close connection\n");
+		}
+		/* Command does not exist */
+		else
+		{
+			printf("Command is not implemented\n Try another commands\n");
+		}
+	}	
+	return 0;
+}
---
+++ serverB.c
@@ -0,0 +1,311 @@
+/*
+ * Authors
+ * Kevin Patrick Jacob
+ * Harmanjot Singh Suri
+ */
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <signal.h>
+
+/*for getting file size using stat()*/
+#include<sys/stat.h>
+
+/*for O_RDONLY*/
+#include<fcntl.h>
+struct stat st = {0};
+
+#define PORT 5092 //if you face a binding error, change the port number
+
+struct sockaddr_in server, client;
+struct stat obj;
+socklen_t sock2, clientLen;
+char buffer[100], command[5], filename[20]; //Character Arrays for handling user inputs
+int k, i, size, c;
+int filehandle, sockRet; //To handle files and sockRet for socket commands
+pid_t childpid; // Child process id
+int cnt = 0; //Client count
+
+/* Handle all incoming commands to the server */
+void ServiceClient(char *command, pid_t *child_pid) {
+    while(1){
+        recv(sock2, buffer, 100, 0);
+        sscanf(buffer, "%s", command);
+            
+        if(!strcmp(command, "STOR"))
+        {
+            int c = 0, clientLen;
+
+            // seperate the command and filename
+            sscanf(buffer+strlen(command), "%s", filename);
+
+            // receive the file size
+            recv(sock2, &size, sizeof(int), 0);
+            i = 1;
+            while(1){
+
+                // Create the file
+                filehandle = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
+                if(filehandle == -1){
+                    sprintf(filename + strlen(filename), "%d", i);
+                }
+                else{
+                    break;
+                }
+            }
+
+            // Dynamically Allocate the size
+            char *f = (char*)malloc(size);
+
+            // Receive the complete file from client
+            recv(sock2, f, size, 0);
+
+            // Write to file
+            c = write(filehandle, f, size);
+
+            // Close the file
+            close(filehandle);
+
+            // send status to client
+            send(sock2, &c, sizeof(int), 0);
+            printf("[+]213 File status OK.\n");
+            printf("[+]200 Command okay.\n");
+        }
+        else if(!strcmp(command, "RNFR"))
+        {
+            char old_name[100], new_name[100];
+
+            // get the old file name
+            recv(sock2, old_name, 100, 0);
+
+            // get new file name
+            recv(sock2, new_name, 100, 0);
+
+            // check if directory is created or not
+            if (stat(buffer, &st) == 1) {
+                printf("[-]Directory Does Not EXIST\n");
+            }
+            else {
+
+                // Rename
+                rename(old_name, new_name);
+                printf("[+]Successfully rename\n");
+            }
+
+            // send status
+            send(sock2, new_name, 100, 0);
+            printf("[+]200 Command okay.\n");
+        }
+
+        else if(!strcmp(command, "RNTO"))
+        {
+            char old_name[100], new_name[100];
+            recv(sock2, old_name, 100, 0);
+
+            recv(sock2, new_name, 100, 0);
+
+            // check if directory is created or not
+            if (stat(buffer, &st) == 1) {
+                
+                printf("[-]Directory Does Not EXIST\n");
+            }
+            else {
+                rename(old_name, new_name);
+                printf("[+]Successfully rename\n");
+            }
+            send(sock2, buffer, 100, 0);
+            printf("[+]200 Command okay.\n");
+        }
+        
+        else if(!(strcmp(command,"CDUP")))
+        {
+            if(chdir("..") == 0)
+                c = 1;
+            else
+                c = 0;
+            send(sock2,&c,sizeof(int),0);
+            printf("[+]200 Command okay");
+        }
+
+        else if(!strcmp(command, "PWD")){
+            system("pwd > PWD.txt");
+            i = 0;
+
+            // Open file in reading mode
+            FILE*f = fopen("PWD.txt", "r");
+
+            // Read until end of file
+            while(!feof(f)){
+
+                // copy the PWD
+                buffer[i++] = fgetc(f);
+            }
+
+            // add the null chracter at the end
+            buffer[i-1] = '\0';
+
+            // close file pointer
+            fclose(f);
+
+            // send the PWD result
+            send(sock2, buffer, 100, 0);
+            printf("[+]200 Command okay.\n");
+        }
+
+        else if(!strcmp(command, "MKD"))
+        {
+            recv(sock2, buffer, 100, 0);
+
+            // check if directory is created or not
+            if (stat(buffer, &st) == -1) {
+
+                // make new directory
+                mkdir(buffer, 0777);
+                
+                printf("257 %s created.\n", buffer);
+            }
+            else {
+                printf("[-]Unable to create directory\n");
+                exit(1);
+            }
+            send(sock2, buffer, 100, 0);
+            printf("[+]200 Command okay.\n");
+        }
+        else if(!strcmp(command, "RMD"))
+        {
+            recv(sock2, buffer, 100, 0);
+
+            // check if directory is created or not
+            if (stat(buffer, &st) == 1) {
+                printf("[-]Directory Does Not EXIST\n");
+            }
+            else {
+                remove(buffer);
+                printf("[+]Successfully remove directory\n");
+            }
+            send(sock2, buffer, 100, 0);
+            printf("[+]200 Command okay.\n");
+        }
+
+        else if(!strcmp(command, "DELE"))
+        {
+            recv(sock2, buffer, 100, 0);
+
+            // check if directory is created or not
+            if (stat(buffer, &st) == 1) {
+                printf("[-]Directory Does Not EXIST\n");
+            }
+            else {
+                remove(buffer);
+                printf("[+]Successfully remove file\n");
+            }
+            send(sock2, buffer, 100, 0);
+            printf("[+]200 Command okay.\n");
+        }
+
+        else if(!strcmp(command, "LIST"))
+        {
+            system("ls > LIST.txt");
+            i = 0;
+            FILE*f = fopen("LIST.txt","r");
+            while(!feof(f)){
+                buffer[i++] = fgetc(f);
+            }
+            buffer[i-1] = '\0';
+            fclose(f);
+            send(sock2, buffer, 1024, 0);
+            printf("[+]200 Command okay.\n");
+        }
+        /* Quit, kill child process, close connection and break from loop */
+        else if(!strcmp(command, "BYE") || !strcmp(command, "QUIT"))
+        {
+            printf("Closing client data connection.\n");
+            i = 1;
+            send(sock2, &i, sizeof(int), 0);
+            close(sock2); 
+            kill(child_pid, SIGKILL);
+            break;
+        }
+
+        else{
+            printf("Command not implemented.\n");
+        }
+    }
+}
+
+int main(int argc,char *argv[])
+{   
+    FILE *fptr;
+	if((sockRet = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("\n------Socket Creation Failed------\n");
+        exit(1);
+    }
+
+	struct sockaddr_in server_addr;
+
+	server_addr.sin_family = AF_INET;
+
+	// Setting port number
+	server_addr.sin_port = htons(PORT); //In case of Binding Error change port number ranging from 1024 to 64000
+
+	// setting IP address
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+
+	// Binding the socket
+	k = bind(sockRet,(struct sockaddr*)&server_addr, sizeof(server_addr));
+	if(k == -1) {
+		printf("[-]Binding error"); //[-] Shows Negative Approach 
+		exit(1);
+	}
+
+	// Listen the connections
+	k = listen(sockRet,10); //At a time socket are open for 10 Connection 
+	if(k == -1){
+		printf("[-]Listen failed");
+		exit(1);
+	}
+
+	while(1){
+		printf("[+]Waiting for incoming connections\n");
+		clientLen = sizeof(client);
+
+		// Accept the incoming connections
+		sock2 = accept(sockRet,(struct sockaddr*)&client, &clientLen);
+
+        // Displaying information of connected client
+        printf("Connection accepted from %s:%d\n",inet_ntoa(client.sin_addr),ntohs(client.sin_port));
+
+        // Print number of clients connected till now
+        printf("Clients connected: %d\n\n",++cnt);
+
+        //Print the count of server A's clients into 
+        //Open file for communication
+        fptr = fopen("/Users/shobhanabhushan/Desktop/ASP_Project/client/serverBComm.txt", "w+");
+        char buf[10];
+        sprintf(buf, "%d", cnt);
+        fwrite(buf, strlen(buf), 1, fptr);
+        fclose(fptr);
+
+		i = 1;
+
+        //Forking and child execution
+        if ((childpid = fork()) == -1) {
+            close(sock2);            
+        } else if(childpid > 0) {
+            close(sock2);
+            continue;
+        } else if(childpid == 0) {
+            printf("\nIn the child process\n");
+            pid_t child_pid = getpid(); 
+            ServiceClient(command, &child_pid);
+        }
+	}
+	return 0;
+}
